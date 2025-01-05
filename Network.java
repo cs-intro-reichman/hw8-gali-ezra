1aad88b9fdff029d8806620d343a64b1f050fd11
galie-ezra <ggaliezra@gmail.com>
2025-01-05 22:46:42 +0200

    first

--- Network.java
+++ Network.java
@@ -22,14 +22,16 @@ public class Network {
         userCount = 3;
     }
 
-    public int getUserCount() {
-        return this.userCount;
-    }
     /** Finds in this network, and returns, the user that has the given name.
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
+
+        for (int i=0; i<users.length;i++) {
+            if (users[i].getName().equals(name)) {
+                return users[i];
+            }
+        }
         return null;
     }
 
@@ -38,7 +40,11 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        //// Replace the following statement with your code
+        if (userCount <= users.length) {
+            users[userCount] = new User(name);
+            userCount++;
+            return true;
+        }
         return false;
     }
 
@@ -46,34 +52,67 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        //// Replace the following statement with your code
+        if (getUser(name1).getfCount()<=10) {
+            getUser(name1).addFollowee(name2);
+            return true;
+
+        }
         return false;
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
-        //// Replace the following statement with your code
-        return null;
+        String reccomendUser="";
+        if (users[0].getName().equals(name)) {
+            reccomendUser = users[1].getName();
+        }
+        else {
+            reccomendUser = users[0].getName();
+        }
+
+        for (int i=0;i<userCount; i++) {
+            if (!users[i].getName().equals(name)) {
+                if (getUser(name).countMutual(users[i]) > getUser(name).countMutual(getUser(reccomendUser))) {
+                    reccomendUser= users[i].getName();
+                }
+            }
+        }
+        return reccomendUser;
     }
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        int indexOfMostPopularUser = 0;
+            for (int i=1;i<userCount;i++) {
+            if (followeeCount(users[i].getName()) > followeeCount(users[indexOfMostPopularUser].getName())) {
+                indexOfMostPopularUser=i;
+            }
+
+        }
+        return users[indexOfMostPopularUser].getName();
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        int followersCount =0;
+        for (int i=0;i<userCount;i++) {
+            if (users[i].follows(name)) {
+                followersCount++;
+            }
+        }
+        return followersCount;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       //// Replace the following statement with your code
-       return null;
+       String description= "";
+        for (int i=0;i<userCount;i++) {
+            description += " "+ users[i].toString();
+       }
+
+       return description;
     }
 }
